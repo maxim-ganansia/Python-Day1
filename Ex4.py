d7a0dc4a1e418045aaa2843e2642484807b41971
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2020-04-05 16:20:57 +0300

    Second commit Exercice day1 python

--- Ex4.py
+++ Ex4.py
@@ -1,2 +1,10 @@
 def is_parsable(x):
-    if x.isdigit
+    if x.isdigit():
+        print("WIN")
+        return True
+    else:
+        print("LOOSE")
+        return False
+
+
+is_parsable(input("user Input : "))
