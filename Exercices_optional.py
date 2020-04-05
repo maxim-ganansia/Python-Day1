6597ad171f721846a9281b03c960d6475e2d33b8
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2020-04-05 20:59:34 +0300

    add ex2 optional

--- Exercices_optional.py
+++ Exercices_optional.py
@@ -5,14 +5,20 @@ def is_parsable(x):
         return False
 
 
-def build_pyramid(input):
+def build_pyramid_with_height_direction(input, direction):
     if is_parsable(input) and int(input) > 0:
         rows = int(input)
-        for i in range(rows):
-            print(" " * (rows - i - 1) + "*" * (2 * i + 1))
+        if direction.lower() == "up":
+            for i in range(rows):
+                print(" " * (rows - i - 1) + "*" * (2 * i + 1))
+        elif direction.lower() == "down":
+            for i in reversed(range(rows)):
+                print(" " * (rows - i - 1) + "*" * (2 * i + 1))
     else:
         print("Please enter a integer greater than 0")
+        exit()
 
 
 rows = input("Please enter the height of our pyramid : ")
-build_pyramid(rows)
+direction = input('please enter a direction of our pyramid : up or down : ')
+build_pyramid_with_height_direction(rows, direction)
