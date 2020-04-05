ceeedf08553512b5bb7e4c805575858c26740de3
Maxim-ilan Ganansia <m.gnansia@gmail.com>
2020-04-05 18:57:39 +0300

    Some changes exercice 5 Python

--- Ex5-Pyramid.py
+++ Ex5-Pyramid.py
@@ -1,8 +1,17 @@
+# def build_pyramid():
+#    s = 9
+#    for i in range(1, 10, 2):
+#        print(" " * s + i * "*")
+#        s = s - 1
+
+
+# build_pyramid()
+
+
 def build_pyramid():
-    s = 9
-    for i in range(1, 10, 2):
-        print(" " * s + i * "*")
-        s = s - 1
+    for i in range(rows):
+        print(" "*(rows - i - 1) + "*" * (2 * i + 1))
 
 
+rows = 5
 build_pyramid()
